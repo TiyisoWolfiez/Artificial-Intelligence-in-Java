29ef74720e36318d3e647402ed0ea0f15edd4c79
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2025-04-13 11:38:24 +0200

    Update Node.java

--- Codes/Assignment 2/src/topaco/model/Node.java
+++ Codes/Assignment 2/src/topaco/model/Node.java
@@ -1,1 +1,25 @@
+package topaco.model;
 
+public class Node {
+    private final double x;
+    private final double y;
+    private final int score;
+
+    public Node(double x, double y, int score) {
+        this.x = x;
+        this.y = y;
+        this.score = score;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
