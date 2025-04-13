f24eff8b054c22aae3fed88356b12ff544a44d8c
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2025-04-13 11:37:49 +0200

    Update DistanceUtil.java

--- Codes/Assignment 2/src/topaco/utils/DistanceUtil.java
+++ Codes/Assignment 2/src/topaco/utils/DistanceUtil.java
@@ -1,1 +1,11 @@
+package topaco.utils;
 
+import topaco.model.Node;
+
+public class DistanceUtil {
+    public static double euclidean(Node a, Node b) {
+        double dx = a.getX() - b.getX();
+        double dy = a.getY() - b.getY();
+        return Math.sqrt(dx * dx + dy * dy)
+    }
+}
