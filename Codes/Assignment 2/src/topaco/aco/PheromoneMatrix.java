dec41bd18895c8d0fadbeba783d808ae9848af92
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2025-04-13 11:41:40 +0200

    Update PheromoneMatrix.java

--- Codes/Assignment 2/src/topaco/aco/PheromoneMatrix.java
+++ Codes/Assignment 2/src/topaco/aco/PheromoneMatrix.java
@@ -1,1 +1,42 @@
+package topaco.aco;
 
+public class PheromoneMatrix {
+    private final double[][] pheromones;
+    private final int size;
+    private final double initialPheromone = 0.1;
+
+    public PheromoneMatrix(int size) {
+        this.size = size;
+        pheromones = new double[size][size];
+        initialize();
+    }
+
+    private void initialize() {
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+                pheromones[i][j] = initialPheromone;
+    }
+
+    public double get(int i, int j) {
+        return pheromones[i][j];
+    }
+
+    public double[][] getMatrix() {
+        return pheromones;
+    }
+
+    public void evaporate(double evaporationRate) {
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+                pheromones[i][j] *= (1 - evaporationRate);
+    }
+
+    public void addPheromone(int i, int j, double amount) {
+        pheromones[i][j] += amount;
+        pheromones[j][i] += amount;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
