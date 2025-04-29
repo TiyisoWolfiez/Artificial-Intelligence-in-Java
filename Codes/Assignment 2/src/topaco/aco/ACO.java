d75af65e71df96d5c7ae36f88ded54e8d03944b9
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2025-04-29 21:26:36 +0200

    Update ACO.java

--- Codes/Assignment 2/src/topaco/aco/ACO.java
+++ Codes/Assignment 2/src/topaco/aco/ACO.java
@@ -1,8 +1,11 @@
 package topaco.aco;
 
 import java.util.Random;
+import java.util.Set;
+
 import topaco.model.ProblemInstance;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class ACO {
@@ -24,93 +27,63 @@ public class ACO {
         this.random = random;
     }
 
-    public void run(int maxIterations) {
-        double bestScore = 0.0;
-        List<Integer> bestRoute = null;
-        double bestDistance = 0.0;
-    
-        for (int iter = 0; iter < maxIterations; iter++) {
-            List<Ant> ants = new ArrayList<>();
-    
-            for (int i = 0; i < instance.getVehicleCount(); i++) {
-                Ant ant = new Ant(instance, pheromones, random);
-                ant.setVehicleIndex(i);
-                ant.constructRoute(pheromones.getMatrix(), alpha, beta);
-                ants.add(ant);
-            }
-    
-            pheromones.evaporate(evaporationRate);
-    
-            for (Ant ant : ants) {
-                List<Integer> route = ant.getVisitedNodes();
-                double contribution = Q / ant.getTotalDistance();
-                for (int i = 0; i < route.size() - 1; i++) {
-                    int from = route.get(i);
-                    int to = route.get(i + 1);
-                    pheromones.addPheromone(from, to, contribution);
-                }
-    
-                if (ant.getTotalScore() > bestScore) {
-                    bestScore = ant.getTotalScore();
-                    bestRoute = new ArrayList<>(route);
-                    bestDistance = ant.getTotalDistance();
-                }
-            }
-    
-            System.out.println("Iteration " + (iter + 1) + " best score: " + bestScore);
-        }
-    
-        System.out.println("\n🏁 Final Best Route: " + bestRoute);
-        System.out.println("Total Distance: " + bestDistance);
-        System.out.println("Total Score: " + bestScore);
-    }    
-
-    public static class Result {
+    public static class ResultPerVehicle {
+        public final int vehicleIndex;
         public final List<Integer> route;
         public final double score;
         public final double distance;
-    
-        public Result(List<Integer> route, double score, double distance) {
+
+        public ResultPerVehicle(int vehicleIndex, List<Integer> route, double score, double distance) {
+            this.vehicleIndex = vehicleIndex;
             this.route = route;
             this.score = score;
             this.distance = distance;
         }
     }
-    
-    public Result runForResult(int maxIterations) {
-        double bestScore = 0.0;
-        List<Integer> bestRoute = null;
-        double bestDistance = 0.0;
-    
-        for (int iter = 0; iter < maxIterations; iter++) {
-            List<Ant> ants = new ArrayList<>();
-    
-            for (int i = 0; i < instance.getVehicleCount(); i++) {
-                Ant ant = new Ant(instance, pheromones, random);
-                ant.setVehicleIndex(i);
-                ant.constructRoute(pheromones.getMatrix(), alpha, beta);
-                ants.add(ant);
+
+    public List<ResultPerVehicle> runForResult(int maxIterations) {
+    int vehicleCount = instance.getVehicleCount();
+    List<ResultPerVehicle> bestResults = new ArrayList<>();
+
+    Set<Integer> visitedGlobal = new HashSet<>();
+
+    for (int v = 0; v < vehicleCount; v++) {
+        bestResults.add(new ResultPerVehicle(v, new ArrayList<>(), 0.0, Double.MAX_VALUE));
+    }
+
+    for (int iter = 0; iter < maxIterations; iter++) {
+        List<Ant> ants = new ArrayList<>();
+
+        for (int i = 0; i < vehicleCount; i++) {
+            Ant ant = new Ant(instance, pheromones, random);
+            ant.setVehicleIndex(i);
+            ant.constructRoute(pheromones.getMatrix(), alpha, beta, visitedGlobal);
+            visitedGlobal.addAll(ant.getVisitedNodes());
+            ants.add(ant);
+        }
+
+        pheromones.evaporate(evaporationRate);
+
+        for (Ant ant : ants) {
+            List<Integer> route = ant.getVisitedNodes();
+            double contribution = Q / ant.getTotalDistance();
+            for (int i = 0; i < route.size() - 1; i++) {
+                int from = route.get(i);
+                int to = route.get(i + 1);
+                pheromones.addPheromone(from, to, contribution);
             }
-    
-            pheromones.evaporate(evaporationRate);
-    
-            for (Ant ant : ants) {
-                List<Integer> route = ant.getVisitedNodes();
-                double contribution = Q / ant.getTotalDistance();
-                for (int i = 0; i < route.size() - 1; i++) {
-                    int from = route.get(i);
-                    int to = route.get(i + 1);
-                    pheromones.addPheromone(from, to, contribution);
-                }
-    
-                if (ant.getTotalScore() > bestScore) {
-                    bestScore = ant.getTotalScore();
-                    bestRoute = new ArrayList<>(route);
-                    bestDistance = ant.getTotalDistance();
-                }
+
+            ResultPerVehicle currentBest = bestResults.get(ant.getVehicleIndex());
+            // Update best results based on the total score and distance
+            if (ant.getTotalScore() > currentBest.score || 
+               (ant.getTotalScore() == currentBest.score && ant.getTotalDistance() < currentBest.distance)) {
+                bestResults.set(ant.getVehicleIndex(), 
+                    new ResultPerVehicle(ant.getVehicleIndex(), new ArrayList<>(route), ant.getTotalScore(), ant.getTotalDistance()));
             }
         }
-    
-        return new Result(bestRoute, bestScore, bestDistance);
     }
+
+    return bestResults;
+}
+   
 }
