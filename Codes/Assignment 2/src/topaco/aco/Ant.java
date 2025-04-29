971a741a4b0903e971f52d399a5ad12465b4a2d9
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2025-04-29 21:27:00 +0200

    Update Ant.java

--- Codes/Assignment 2/src/topaco/aco/Ant.java
+++ Codes/Assignment 2/src/topaco/aco/Ant.java
@@ -20,35 +20,36 @@ public class Ant {
         this.instance = instance;
         this.pheromones = pheromones;
         this.random = random;
-        this.vehicleIndex = -1;
+        this.vehicleIndex = 0;
         this.visitedNodes = new ArrayList<>();
         this.visitedSet = new HashSet<>();
         this.totalDistance = 0.0;
         this.totalScore = 0.0;
     }
 
-    public void constructRoute(double[][] pheromones, double alpha, double beta) {
+    public void constructRoute(double[][] pheromones, double alpha, double beta, Set<Integer> visitedGlobal) {
         int n = instance.getNodes().size();
-        int current = 0;
+        int current = 0; // start at depot
         visitedNodes.add(current);
         visitedSet.add(current);
+        visitedGlobal.add(current);
 
         while (true) {
-            int next = selectNextNode(current, pheromones, alpha, beta);
+            int next = selectNextNode(current, pheromones, alpha, beta, visitedGlobal);
             if (next == -1) break;
-
             double dist = DistanceUtil.euclidean(instance.getNode(current), instance.getNode(next));
             if (totalDistance + dist + getReturnDistance(next) > instance.getTmax()) break;
-
+    
             totalDistance += dist;
             totalScore += instance.getNode(next).getScore();
-
+    
             visitedNodes.add(next);
             visitedSet.add(next);
+            visitedGlobal.add(next);
+    
             current = next;
         }
-
-        // return to depot
+    
         totalDistance += getReturnDistance(current);
         visitedNodes.add(0);
     }
@@ -57,19 +58,19 @@ public class Ant {
         return DistanceUtil.euclidean(instance.getNode(from), instance.getNode(0));
     }
 
-    private int selectNextNode(int current, double[][] pheromones, double alpha, double beta) {
+    private int selectNextNode(int current, double[][] pheromones, double alpha, double beta, Set<Integer> visitedGlobal) {
         List<Integer> candidates = new ArrayList<>();
         for (int i = 1; i < instance.getNodes().size(); i++) {
-            if (!visitedSet.contains(i)) {
+            if (!visitedSet.contains(i) && !visitedGlobal.contains(i)) {
                 candidates.add(i);
             }
         }
-
+    
         if (candidates.isEmpty()) return -1;
-
+    
         double[] probabilities = new double[candidates.size()];
         double sum = 0.0;
-
+    
         for (int i = 0; i < candidates.size(); i++) {
             int j = candidates.get(i);
             double tau = Math.pow(pheromones[current][j], alpha);
@@ -77,21 +78,19 @@ public class Ant {
             probabilities[i] = tau * eta;
             sum += probabilities[i];
         }
-
-        if (sum == 0.0) return -1;
-
-        double rand = Math.random() * sum;
+    
+        double rand = random.nextDouble() * sum;
         double cumulative = 0.0;
-
         for (int i = 0; i < candidates.size(); i++) {
             cumulative += probabilities[i];
             if (rand <= cumulative) {
                 return candidates.get(i);
             }
         }
-
+    
         return candidates.get(candidates.size() - 1);
     }
+    
 
     private double getHeuristic(int from, int to) {
         Node node = instance.getNode(to);
